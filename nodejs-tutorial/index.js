0596cae59a58487c14c9354bc2530257315ecea6
yakintech <cagatayyildiz87@gmail.com>
2021-12-01 21:12:56 +0300

    http server operation

---
+++ nodejs-tutorial/index.js
@@ -0,0 +1,47 @@
+const http = require('http')
+const fs = require('fs')
+
+const port = 8080
+const server = http.createServer(function (req, res) {
+    //throw 'aman da aman';
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    // res.end('Hello Nodejs!')
+
+    if (req.url == '/') {
+        fs.readFile('./index.html', function (err, data) {
+            if( err == null){
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data);
+            }
+            else{
+                res.writeHead(500);
+                res.end('Sunucu tarafında bir hata meydana geldi')
+            }
+        })
+    }
+    else if (req.url == '/about') {
+
+        fs.readFile('./about2.html', function(err,data){
+
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data)
+
+        })
+    }
+    else {
+        fs.readFile('./404.html', function(err,data){
+
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data)
+
+        })
+    }
+
+
+})
+
+
+server.listen(port, function () {
+    console.log("Sunucum çalışıyor...!");
+})
+
